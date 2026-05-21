beba9180702a3db3b41aff0b2fc3627e2f66ddd0
NervGear <nervgear@NervGeardeMacBook-Air.local>
2026-05-21 12:17:47 +0900

    fix socket server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, dir: __dirname });
 const handle = app.getRequestHandler();
 
+const PORT = process.env.PORT || 3000;
+
 const rooms = {};
 
 function makeRoomCode() {
@@ -14,23 +16,28 @@ function makeRoomCode() {
 
 app.prepare().then(() => {
   const server = http.createServer((req, res) => {
-    return handle(req, res);
+    handle(req, res);
   });
 
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
 
   io.on("connection", (socket) => {
     console.log("connected:", socket.id);
 
     socket.on("createRoom", () => {
       const code = makeRoomCode();
+
       rooms[code] = {
         code,
         players: [{ id: socket.id, name: "Player 1" }],
-        gameState: null,
       };
 
       socket.join(code);
+
       io.to(code).emit("roomUpdate", rooms[code]);
     });
 
@@ -47,26 +54,21 @@ app.prepare().then(() => {
         return;
       }
 
-      rooms[code].players.push({ id: socket.id, name: "Player 2" });
+      rooms[code].players.push({
+        id: socket.id,
+        name: "Player 2",
+      });
+
       socket.join(code);
+
       io.to(code).emit("roomUpdate", rooms[code]);
-
-      if (rooms[code].gameState) {
-        socket.emit("gameSync", rooms[code].gameState);
-      }
-    });
-
-    socket.on("gameSync", ({ roomCode, state }) => {
-      const code = String(roomCode || "").toUpperCase();
-      if (!rooms[code]) return;
-
-      rooms[code].gameState = state;
-      socket.to(code).emit("gameSync", state);
     });
 
     socket.on("disconnect", () => {
       for (const code of Object.keys(rooms)) {
-        rooms[code].players = rooms[code].players.filter((p) => p.id !== socket.id);
+        rooms[code].players = rooms[code].players.filter(
+          (p) => p.id !== socket.id
+        );
 
         if (rooms[code].players.length === 0) {
           delete rooms[code];
@@ -77,7 +79,7 @@ app.prepare().then(() => {
     });
   });
 
-  server.listen(3000, () => {
-    console.log("Ready on http://localhost:3000");
+  server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on ${PORT}`);
   });
 });
